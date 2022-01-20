903fab9e1708ba931a9aa6575516a7091d339b05
Smlcrp <smlcrp@gmail.com>
2022-01-20 15:56:13 -0500

    connected MongoDB and Express with mongoose

---
+++ controllers/productController.js
@@ -0,0 +1,4 @@
+const express = require('express');
+const router = express.Router();
+const products = require('../models/productModel.js')
+require('./config/db.connection')
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-
+require('./config/db.connection')
 
 
 const PORT = 3000;
