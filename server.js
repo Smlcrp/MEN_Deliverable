dc31c7975ce7c772ed2a269949b2e42d2a394931
Smlcrp <smlcrp@gmail.com>
2022-01-20 13:00:30 -0500

    added form to new.ejs, updated new route to render new.ejs

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.get("/", function(req, res) {
 })
 
 app.get("/new", function(req, res) {
-    res.send("This new page is working!")
+    res.render("new.ejs")
 })
         
 app.get("/*", (req, res) => {
