c6c511ed80b5bbe23fbecd91a1d54937428298c2
Smlcrp <smlcrp@gmail.com>
2022-01-20 12:34:28 -0500

    made working express app

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+
+
+
+const PORT = 3000;
+
+
+app.get("/", function(req, res) {
+    res.send("This is working! :)")
+})
+        
+app.get("/*", (req, res) => {
+    const context = { error: req.error };
+    return res.status(404).render("404", context);
+});
+
+app.listen(PORT, function() {
+    console.log(`I am listening on port ${PORT}`)
+});
